20b45ecb528bbc655f12881b25046c1850644efb
nikhil <109782243+ElegantFalcon@users.noreply.github.com>
2023-05-21 21:17:45 +0530

    V3

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,13 +4,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDSFo8iMLprNgFKzTNJ7WeFi03CPnKMYDc",
-  authDomain: "baseapp-31776.firebaseapp.com",
-  projectId: "baseapp-31776",
-  storageBucket: "baseapp-31776.appspot.com",
-  messagingSenderId: "437626336544",
-  appId: "1:437626336544:web:e3180cad3fec7838ba1345",
-  measurementId: "G-X6PJWY4JJ1"
+  
 };
 
 const app = initializeApp(firebaseConfig);
